d19031cc531b160a35698b2c0e0006ffaa93dd57
nadeshda18 <nadeshda02@hotmail.com>
2024-12-11 17:31:57 -0400

    who is 2

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^Registrant |^Admin |^Tech / {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $1 "," $2}' > "$1.csv"
+whois $1 | awk -F': ' '/^Registrant |^Admin |^Tech / {print $1 "," $2}' > "$1.csv"
