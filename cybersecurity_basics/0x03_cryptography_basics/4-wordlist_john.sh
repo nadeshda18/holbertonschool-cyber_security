baf23046019290a5b13e0d529cd5082e7e5616ff
nadeshda18 <nadeshda02@hotmail.com>
2025-02-20 00:57:50 +0100

    what a fight

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=rockyou.txt "$1" && john --show "$1" | awk -F: '{print $2}' > 4-password.txt
+john --wordlist=/home/nadeshda/Documents/rockyou.txt --format=Raw-MD5 "$1" && john --show --format=Raw-MD5 "$1" | awk -F: '{print $2}' > 4-password.txt
